acb737f142e27b19f1eb07cc5d0a8dca56e0ae70
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-11-16 14:21:06 -0500

    Cambios de configuración de webpack&babel

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+module.exports = {
+	mode: 'production',
+    entry : './src/app/index.js', 
+    output : {
+        filename : 'main.js', 
+        path : path.resolve(__dirname, 'build')
+    }
+}
