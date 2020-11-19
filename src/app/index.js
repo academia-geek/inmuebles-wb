f01695394beb91ec910b83c1fd30b8e60aafdf78
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-11-19 09:55:01 -0500

    cambios

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,3 @@
 console.log("Hola desde webpack!!!!!")
+let a = 7
+	
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
 	mode: 'development',
     entry : './src/app/index.js', 
     output : {
         filename : 'main.js', 
         path : path.resolve(__dirname, 'build')
-    }
+    }, 
+    plugins : [
+        new HtmlWebpackPlugin({template : './src/index.html'})
+    ]    
 }
