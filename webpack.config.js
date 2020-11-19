a2575411d166a12e556d322fcf46bf78f0d950dd
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-11-19 10:02:52 -0500

    cambios

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-	mode: 'development',
-    entry : './src/app/index.js', 
-    output : {
-        filename : 'main.js', 
-        path : path.resolve(__dirname, 'build')
-    }, 
-    plugins : [
-        new HtmlWebpackPlugin({template : './src/index.html'})
-    ]    
+    mode: 'development',
+    entry: './src/app/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', 
+            minify : true
+        })
+
+
+    ]
 }
